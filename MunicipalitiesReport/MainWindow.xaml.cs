0b3842cfa2ab8c186bc790cd6799cad900b5ad47
OrtizBrayan <brayanortizbastidas@gmail.com>
2022-01-29 13:53:22 -0500

    Create principal class, municipality.

--- MunicipalitiesReport/MainWindow.xaml.cs
+++ MunicipalitiesReport/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MunicipalitiesReport
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ MunicipalitiesReport/src/Municipality.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MunicipalitiesReport.src
+{
+    public class Municipality
+    {
+        private String region;
+        private String codeOfRegion;
+        private String department;
+        private String codeOfDepart;
+        private String municipalityName;
+
+        public Municipality(String r, String cor, String dpt, String cfd, String name)
+        {
+            region = r;
+            codeOfRegion = cor;
+            codeOfDepart = dpt;
+            codeOfDepart = cfd;
+            municipalityName = name;
+        }
+
+        public string Region { get => region; set => region = value; }
+        public string CodeOfRegion { get => codeOfRegion; set => codeOfRegion = value; }
+        public string Department { get => department; set => department = value; }
+        public string CodeOfDepart { get => codeOfDepart; set => codeOfDepart = value; }
+        public string MunicipalityName { get => municipalityName; set => municipalityName = value; }
+    }
+}
